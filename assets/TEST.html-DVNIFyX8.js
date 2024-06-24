2021e5c626a6fa2a3b36ddbbe3b9cec18c34c045
guhaochong <52439133+guhaochong@users.noreply.github.com>
2024-06-24 04:40:12 +0000

    Deploying to gh-pages from @ guhaochong/Cloudflare-JinShan-RelayProtection@2a007ebf17338272449ce02ec7f3a8f51c9ab627 🚀

---
+++ assets/TEST.html-DVNIFyX8.js
@@ -0,0 +1,18 @@
+import{_ as c,r as l,c as r,a as e,b as a,w as n,e as o,o as d,d as s}from"./app-Cooc7pjz.js";const p={},u=o('<h1 id="test-测试" tabindex="-1"><a class="header-anchor" href="#test-测试"><span>TEST 测试</span></a></h1><div class="custom-container tip"><p class="custom-container-title">TIP</p><p>这是一个提示</p></div><div class="custom-container warning"><p class="custom-container-title">WARNING</p><p>这是一个警告</p></div><div class="custom-container danger"><p class="custom-container-title">DANGER</p><p>这是一个危险警告</p></div><details class="custom-container details"><p>这是一个详情块，在 Internet Explorer / Edge 中不生效</p></details>',5),i={class:"table-of-contents"},b=o(`<table><thead><tr><th></th><th></th></tr></thead><tbody><tr><td>::: tip这是一个提示:::</td><td></td></tr></tbody></table><h2 id="遥信点表" tabindex="-1"><a class="header-anchor" href="#遥信点表"><span>遥信点表</span></a></h2><blockquote><pre><code>  点号	1
+</code></pre><blockquote><pre><code>	事故总信号 	
+</code></pre><blockquote><pre><code>测试2	
+</code></pre></blockquote></blockquote></blockquote><blockquote><pre><code>  点号	2
+</code></pre><blockquote><pre><code>	10千伏一段自切未准备	
+</code></pre><blockquote><pre><code>测试2	
+</code></pre></blockquote></blockquote></blockquote><blockquote><pre><code>  点号	3
+</code></pre><blockquote><pre><code>	10千伏二段自切未准备	
+</code></pre><blockquote><pre><code>测试2	
+</code></pre></blockquote></blockquote></blockquote><blockquote><pre><code>  点号	4
+</code></pre><blockquote><pre><code>	10千伏分段自切跳一段进线开关	
+</code></pre><blockquote><pre><code>自切动作后，跳开一号进线开关信号	
+</code></pre></blockquote></blockquote></blockquote><blockquote><pre><code>  点号	5
+</code></pre><blockquote><pre><code>	1号站用变成套柜高压侧接地闸刀 （合）	
+</code></pre></blockquote></blockquote><blockquote><pre><code>  点号	6
+</code></pre><blockquote><pre><code>	10千伏B串自愈保护动作	
+</code></pre><blockquote><pre><code>B串自愈保护动作信号（母差，解列等保护动作软报文信号）	
+</code></pre></blockquote></blockquote></blockquote>`,8);function k(q,h){const t=l("router-link");return d(),r("div",null,[u,e("nav",i,[e("ul",null,[e("li",null,[a(t,{to:"#遥信点表"},{default:n(()=>[s("遥信点表")]),_:1})])])]),b])}const _=c(p,[["render",k],["__file","TEST.html.vue"]]),T=JSON.parse('{"path":"/Page/TEST.html","title":"TEST 测试","lang":"zh-CN","frontmatter":{},"headers":[{"level":2,"title":"遥信点表","slug":"遥信点表","link":"#遥信点表","children":[]}],"git":{"updatedTime":1719203980000,"contributors":[{"name":"guhaochong","email":"guhaochong@outlook.com","commits":2}]},"filePathRelative":"Page/TEST.md"}');export{_ as comp,T as data};
